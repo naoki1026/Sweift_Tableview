a89b66eab4b4bc697276d13256f7f19ba6c1bcbb
Naoki Arakawae <naoki.arakawa1026@gmail.com>
2019-02-20 19:44:30 +0900

    first commit

--- Swift5TableView1/ViewController.swift
+++ Swift5TableView1/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var box = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+    
+    
     }
 
+    
+    @IBAction func countUp(_ sender: Any) {
+        
+        box += 1
+        label.text = String(box)
+        
+        if (box > 10) {
+            
+            //boxが10を超えた場合に画面を遷移させる
+            let viewController2 : ViewController2 = self.storyboard?.instantiateViewController(withIdentifier : "next") as! ViewController2
+            
+            viewController2.count = box
+            
+            //画面遷移
+            self.navigationController?.pushViewController(viewController2, animated: true)
+            
+            
+        }
+        
+        
+    }
+    
 
 }
 
---
+++ Swift5TableView1/ViewController3.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController3.swift
+//  Swift5TableView1
+//
+//  Created by Naoki Arakawa on 2019/02/20.
+//  Copyright © 2019 Naoki Arakawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+    
+    
+    @IBOutlet weak var label2: UILabel!
+    
+    var count3 = 0
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        label2.text = String(count3)
+
+      
+    }
+    
+
+    
+
+}
